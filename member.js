2c5b1308b7c4131abb0224c419731d6ac423a7e8
Praveen M <praveenm7111@gmail.com>
2024-09-29 06:45:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        { name: 'JavaScript', level: 'Advanced' },
+        { name: 'C#', level: 'Intermediate' },
+        { name: 'SQL', level: 'Intermediate' },
+        { name: 'HTML', level: 'Advanced' },
+        { name: 'CSS', level: 'Advanced' },
+        { name: 'AngularJS', level: 'Intermediate' },
+        { name: 'ASP.NET', level: 'Intermediate' }
+    ];
+}
